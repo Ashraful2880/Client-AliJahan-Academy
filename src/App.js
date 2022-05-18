d76e3bd02378938b09297e8afc841c6aca2a6542
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-18 22:16:12 +0600

    Header And React router Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Footer from './Components/Common/Footer/Footer';
+import Header from './Components/Common/Header/Header';
+import Home from './Components/Home/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is app Area</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/*' element={<NotFound />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Home/Bannar/Bannar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bannar = () => {
+    return (
+        <div className='min-h-[80vh]'>
+            This is Bannar
+        </div>
+    );
+};
+
+export default Bannar;
