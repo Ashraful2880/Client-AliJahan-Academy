63578a19f088161b37c4dcbb7186d8697a37f2b5
Ashraful2880 <ashrafulislam2880@gmail.com>
2022-05-20 13:19:37 +0600

    Admission Details Page Design Added

--- src/Components/Admissions/Admissions.js
+++ src/Components/Admissions/Admissions.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Admissions = () => {
     return (
-        <div>
-            <div className="container mx-auto h-[80vh]">
+        <>
+            <div className="container mx-auto mb-8">
                 <h1 className="text-4xl text-orange-500 font-bold">Our Curriculum</h1>
                 <h4 className="text-xl font-semibold mb-6">Academic Programme</h4>
                 <p className=" tracking-wider">Massa, et porttitor cras sed ultrices ullamcorper mauris semper iaculis. Ornare suscipit purus nulla</p>
@@ -13,8 +14,80 @@ const Admissions = () => {
                     <img src="https://res.cloudinary.com/ashraful-islam/image/upload/v1652965045/Assesment/Admission-2_yldpb6.jpg" alt="Admission-2" />
                     <img src="https://res.cloudinary.com/ashraful-islam/image/upload/v1652965045/Assesment/Admission-3_htzrvx.jpg" alt="Admission-3" />
                 </div>
+                <h1 className="text-3xl font-semibold mt-6">Apply For Admission</h1>
             </div>
-        </div>
+            <div className="container mx-auto">
+                <div className="flex justify-center px-6 mb-12">
+                    <div className="w-full xl:w-3/4 lg:w-11/12 flex">
+                        <div className="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg bgContainer"></div>
+                        <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+                            <h3 className="pt-4 text-2xl text-center">Add Your Deatils For Admission</h3>
+                            <hr className="mt-4 border-t" />
+                            <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded" >
+                                <div className="mb-4 md:flex md:justify-between">
+                                    <div className="mb-4 md:mr-2 md:mb-0 text-left">
+                                        <label
+                                            className="block mb-2 text-sm font-bold text-gray-700" htmlFor="name">
+                                            Your Name
+                                        </label>
+                                        <input
+                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            type="text"
+                                            name="name"
+                                            placeholder="Enter Your Name"
+                                        />
+                                    </div>
+                                    <div className="md:ml-2 text-left">
+                                        <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="number">
+                                            Contact Number
+                                        </label>
+                                        <input
+                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            name="number"
+                                            type="number"
+                                            placeholder="Contact Number"
+                                        />
+                                    </div>
+                                </div>
+                                <div className="mb-4 md:flex md:justify-between">
+                                    <div className="mb-4 md:mr-2 md:mb-0 text-left">
+                                        <label
+                                            className="block mb-2 text-sm font-bold text-gray-700" htmlFor="class">
+                                            Admit Class Name
+                                        </label>
+                                        <input
+                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            type="text"
+                                            name="class"
+                                            placeholder="Which Class Wat to Admit?"
+                                        />
+                                    </div>
+                                    <div className="md:ml-2 text-left">
+                                        <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="email">
+                                            Your Email (Optional)
+                                        </label>
+                                        <input
+                                            className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                                            name="email"
+                                            type="email"
+                                            placeholder="Enter Email"
+                                        />
+                                    </div>
+                                </div>
+                                <hr className="mb-12 border-t" />
+                                <div className="mb-6 text-center">
+                                    <button
+                                        className="w-full px-4 py-2 font-bold text-white bg-orange-500 rounded-full hover:bg-orange-700 focus:outline-none focus:shadow-outline"
+                                        type="submit">
+                                        Apply For Admission
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
